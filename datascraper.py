d797f2b37253a839791299cdad18e3022b9ca664
Nermeen Nedal <nermeennedal11@gmail.com>
2026-03-12 23:59:48 +0300

    Update datascraper.py

--- datascraper.py
+++ datascraper.py
@@ -11,5 +11,14 @@ def work(Datapage):
     soup=BeautifulSoup(Datapage.content,"lxml")
     champoion =soup.find_all("div",{'class':'matchCard'}) 
     Matches=[]
-    print(champoion)
-work(Datapage)
+    return champoion
+
+def content(Datapage):
+    dictionary=work(Datapage)
+    list1 = dictionary.contents[0]
+    return list1
+
+def main():
+    print(content(Datapage))
+
+main()
