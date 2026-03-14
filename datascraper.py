9474e400e65a62ac23677ea6c7aed0f5670da422
Nermeen Nedal <nermeennedal11@gmail.com>
2026-03-14 23:17:57 +0300

    initial

--- datascraper.py
+++ datascraper.py
@@ -7,14 +7,59 @@ date = input("Please enter the date in the following format DD/MM/YYYY : ")
 
 Datapage= requests.get(f"https://www.yallakora.com/match-center?date={date}#days")
 
-def work(Datapage):
-    soup=BeautifulSoup(Datapage.content,"lxml")
-    champoions =soup.find_all("div",{'class':'matchCard'}) 
-    Matches=[]
-    x=champoions[0].contents[0].find('h2').text
-    return x
+def Soup(data):
+    cont = data.content
+    contentsoup = BeautifulSoup(cont,"lxml")
+    champoions= contentsoup.find_all("div",{'class':'matchCard'})
+    return champoions
 
-print(work(Datapage))
+def get_match_info(champoions):
+    champoions=Soup(champoions)
+    champion=champoions[0].contents[0]
+    return champion
+
+print(get_match_info(Datapage))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def work(Datapage):
+#     soup=BeautifulSoup(Datapage.content,"lxml")
+#     champoions =soup.find_all("div",{'class':'matchCard'}) 
+#     Matches=[]
+#     x=champoions[0].contents[0].find('h2').text
+#     return x
+
+# print(work(Datapage))
 
 
 # def content(Datapage):
